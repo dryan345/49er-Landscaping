4c933f50eb10a7e4aabd58a72bd535ac1ad0fa7a
Devin Ryan <dryan4@student.sierracollege.edu>
2017-10-27 01:07:25 -0700

    Added slideshow, small info section, and changed color scheme

---
+++ slider.js
@@ -0,0 +1,12 @@
+// Slick Slideshow
+$(document).ready(function() {
+  $('.container-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    arrows: true,
+    pauseOnHover: false,
+  });
+});
